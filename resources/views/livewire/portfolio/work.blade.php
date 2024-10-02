1e2dc1c86dd1ab8859e5d6cc7b410a618b99ecdb
Nick Dillon <nickhds@gmail.com>
2024-10-02 00:11:15 -0500

    Update work experience

--- resources/views/livewire/portfolio/work.blade.php
+++ resources/views/livewire/portfolio/work.blade.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Carbon\Carbon;
 use Livewire\Volt\Component;
 
@@ -30,8 +32,8 @@ new class extends Component {
         return [
             'work_experience' => [
                 'gray' => [
-                    'url' => 'https://gray.tv/',
-                    'company' => 'Gray Television',
+                    'url' => 'http://graymedia.com/',
+                    'company' => 'Gray Media',
                     'position' => 'Digital Applications Developer',
                     'time' => 'April 2023 - Present',
                     'duration' => $this->calculateDuration('2023-04-24'),
@@ -59,35 +61,35 @@ new class extends Component {
         <div class="w-10/12 px-2 py-8 rounded-md text-slate-50">
             <ul class="relative border-l-2 border-slate-700 space-y-14">
                 @foreach ($work_experience as $work)
-                    <li class="ml-6">
-                        <span
-                            class="absolute flex items-center justify-center w-8 h-8 rounded-full -left-4 ring-8 ring-[#18192c] bg-[#18192c]">
-                            <img class="shadow-lg" src="{{ asset($work['image']) }}" />
-                        </span>
+                <li class="ml-6">
+                    <span
+                        class="absolute flex items-center justify-center w-8 h-8 rounded-full -left-4 ring-8 ring-[#18192c] bg-[#18192c]">
+                        <img class="shadow-lg" src="{{ asset($work['image']) }}" />
+                    </span>
 
-                        <div class="ml-2 shadow-sm sm:grid sm:grid-cols-2 sm:items-center">
-                            <div>
-                                <a class="duration-300 ease-in-out cursor-pointer hover:text-slate-400"
-                                    href="{{ $work['url'] }}" target="_blank">
-                                    <h1 class="text-xl font-semibold">
-                                        {{ $work['company'] }}
-                                    </h1>
-                                </a>
+                    <div class="ml-2 shadow-sm sm:grid sm:grid-cols-2 sm:items-center">
+                        <div>
+                            <a class="duration-300 ease-in-out cursor-pointer hover:text-slate-400"
+                                href="{{ $work['url'] }}" target="_blank">
+                                <h1 class="text-xl font-semibold">
+                                    {{ $work['company'] }}
+                                </h1>
+                            </a>
 
-                                <h2 class="text-sm">
-                                    {{ $work['position'] }}
-                                </h2>
-                            </div>
-
-                            <div class="flex mt-1 text-xs font-normal sm:flex-col sm:items-center sm:pl-24">
-                                <span>{{ $work['time'] }}</span>
-
-                                <span class="block px-1 sm:hidden">•</span>
-
-                                <span>{{ $work['duration'] }}</span>
-                            </div>
+                            <h2 class="text-sm">
+                                {{ $work['position'] }}
+                            </h2>
                         </div>
-                    </li>
+
+                        <div class="flex mt-1 text-xs font-normal sm:flex-col sm:items-center sm:pl-24">
+                            <span>{{ $work['time'] }}</span>
+
+                            <span class="block px-1 sm:hidden">•</span>
+
+                            <span>{{ $work['duration'] }}</span>
+                        </div>
+                    </div>
+                </li>
                 @endforeach
             </ul>
         </div>
