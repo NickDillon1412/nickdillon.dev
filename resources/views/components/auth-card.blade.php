0776e344ca134a7ec73c92daa833cef57ae364b7
Nick Dillon <nickhds@gmail.com>
2024-10-02 18:26:31 -0500

    Set up GitHub sign up/login

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,15 +1,15 @@
 @props(['submit', 'header' => null, 'route' => null, 'link' => null])
 
 <div
-    class="w-full max-w-[30rem] p-2 flex flex-col gap-2 rounded-2xl border border-gray-200 dark:border-gray-700 bg-gray-100 dark:bg-gray-900">
-    <div class="flex flex-col p-6 bg-white shadow-sm rounded-xl md:p-8 dark:bg-gray-800">
+    class="w-full max-w-[30rem] p-2 flex flex-col gap-2 rounded-2xl border border-slate-200 dark:border-slate-700 bg-slate-100/50 dark:bg-slate-900">
+    <div class="flex flex-col p-6 bg-white shadow-sm rounded-xl md:p-8 dark:bg-slate-800">
         {{ $header }}
 
         <form wire:submit="{{ $submit }}">
             {{ $content }}
 
             <div class="flex items-center justify-between mt-4">
-                <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                <a class="text-sm underline rounded-md text-slate-600 dark:text-slate-400 hover:text-slate-900 dark:hover:text-slate-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-slate-800"
                     href="{{ $link?->attributes['route'] }}" wire:navigate>
                     {{ $link }}
                 </a>
@@ -21,5 +21,25 @@
                 </div>
             </div>
         </form>
+
+        <flux:separator text="Or continue with" class="my-8" />
+
+        <div class="flex items-center space-x-2">
+            <flux:button variant="outline" href="{{ route('movie-vault.wishlist') }}" class="w-full">
+                <x-bi-google class="w-5 h-5" />
+            </flux:button>
+
+            <flux:button variant="outline" href="{{ route('github.redirect') }}" class="w-full">
+                <x-bi-github class="w-5 h-5" />
+            </flux:button>
+
+            <flux:button variant="outline" href="{{ route('movie-vault.wishlist') }}" class="w-full">
+                <x-bi-apple class="w-5 h-5" />
+            </flux:button>
+
+            <flux:button variant="outline" href="{{ route('movie-vault.wishlist') }}" class="w-full">
+                <x-fab-x-twitter class="w-5 h-5" />
+            </flux:button>
+        </div>
     </div>
 </div>
