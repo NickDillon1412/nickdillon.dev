126d315a8029aaf09e024962d83244c39acf91ca
Nick Dillon <nickhds@gmail.com>
2024-12-02 23:42:26 -0600

    Add ability to show/hide passwords

--- resources/views/livewire/pages/auth/sign-up.blade.php
+++ resources/views/livewire/pages/auth/sign-up.blade.php
@@ -36,7 +36,7 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
+<div x-data="{ showPassword: false, showPasswordConfirmation: false }">
     <x-auth-card submit="signup">
         <x-slot:header>
             <h1 class="flex justify-center mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-100">
@@ -62,19 +62,34 @@ new #[Layout('layouts.guest')] class extends Component
             <!-- Password -->
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
-                <x-text-input wire:model="password" id="password" class="block w-full mt-1"
-                    type="password"
-                    name="password"
-                    required autocomplete="new-password" />
+
+                <div class="relative pt-0.5">
+                    <input :type="showPassword ? 'text' : 'password'" id="password" name="password" wire:model="password" class="block w-full rounded-md shadow-sm border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600" autocomplete="new-password" required />
+
+                    <button x-cloak x-on:click="showPassword = !showPassword" class="absolute inset-y-0 top-0 flex items-center end-4" type="button">
+                        <flux:icon x-show="!showPassword && $wire.password.length" icon="eye" variant="outline" class="w-5 h-5" />
+
+                        <flux:icon x-show="showPassword && $wire.password.length" icon="eye-slash" variant="outline" class="w-5 h-5" />
+                    </button>
+                </div>
+
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
 
             <!-- Confirm Password -->
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-                <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block w-full mt-1"
-                    type="password"
-                    name="password_confirmation" required autocomplete="new-password" />
+
+                <div class="relative pt-0.5">
+                    <input :type="showPasswordConfirmation ? 'text' : 'password'" id="password_confirmation" name="password_confirmation" wire:model="password_confirmation" class="block w-full rounded-md shadow-sm border-slate-300 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600" autocomplete="new-password" required />
+
+                    <button x-cloak x-on:click="showPasswordConfirmation = !showPasswordConfirmation" class="absolute inset-y-0 top-0 flex items-center end-4" type="button">
+                        <flux:icon x-show="!showPasswordConfirmation && $wire.password_confirmation.length" icon="eye" variant="outline" class="w-5 h-5" />
+
+                        <flux:icon x-show="showPasswordConfirmation && $wire.password_confirmation.length" icon="eye-slash" variant="outline" class="w-5 h-5" />
+                    </button>
+                </div>
+
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
         </x-slot:content>
