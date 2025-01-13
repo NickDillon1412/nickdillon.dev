8a8028cb090846471e755718e397352dafd42deb
Nick Dillon <nickhds@gmail.com>
2025-01-13 09:30:13 -0600

    Update validation message for category

--- resources/views/components/pure-finance/categories.blade.php
+++ resources/views/components/pure-finance/categories.blade.php
@@ -14,7 +14,7 @@
         return category ? category.name : 'Select a category';
     }
 }" wire:ignore>
-    <p class="cursor-default block text-sm font-medium text-slate-700 dark:text-slate-300">
+    <p class="block text-sm font-medium cursor-default text-slate-700 dark:text-slate-300">
         Category
 
         <span class="text-rose-500">*</span>
@@ -22,14 +22,14 @@
 
     <div class="relative inline-flex w-full">
         <div class="flex items-stretch w-full mt-2">
-            <div
-                class="flex items-center w-full text-sm text-left rounded-lg ring-1 shadow-sm border-slate-300 dark:border-slate-700 dark:bg-slate-900 form-input py-[9px] dark:text-slate-300 z-20"
+            <div class="flex items-center w-full text-sm text-left rounded-lg ring-1 shadow-sm border-slate-300 dark:border-slate-700 dark:bg-slate-900 form-input py-[9px] dark:text-slate-300 z-20"
                 :class="{ '!border-indigo-500 dark:!border-indigo-600 ring-1 !ring-indigo-500 dark:!ring-indigo-600': showDropdown }">
-                <button type="button" class="flex items-center justify-between py-0 w-full" aria-haspopup="true" x-on:click="showDropdown = true"
-                    :aria-expanded="showDropdown" aria-expanded="false">
+                <button type="button" class="flex items-center justify-between w-full py-0" aria-haspopup="true"
+                    x-on:click="showDropdown = true" :aria-expanded="showDropdown" aria-expanded="false">
                     <p class="-my-[1px] text-slate-600 dark:text-slate-300" x-text="selectedCategoryName"></p>
 
-                    <svg class="ml-1 fill-current shrink-0 text-slate-500" width="11" height="7" viewBox="0 0 11 7">
+                    <svg class="ml-1 fill-current shrink-0 text-slate-500" width="11" height="7"
+                        viewBox="0 0 11 7">
                         <path d="M5.4 6.8L0 1.4 1.4 0l4 4 4-4 1.4 1.4z"></path>
                     </svg>
                 </button>
@@ -62,7 +62,8 @@
                     </div>
 
                     <div class="absolute flex items-center -space-x-0.5 pr-1 inset-0 left-auto">
-                        <button x-cloak x-show="search.length > 0" x-on:click="search = ''" type="button" aria-label="Search">
+                        <button x-cloak x-show="search.length > 0" x-on:click="search = ''" type="button"
+                            aria-label="Search">
                             <x-heroicon-s-x-mark
                                 class="w-6 h-6 p-0.5 text-rose-500 duration-200 ease-in-out rounded-md hover:bg-slate-200 dark:hover:bg-slate-700" />
                         </button>
@@ -74,11 +75,13 @@
                 <div class="px-1 overflow-y-scroll max-h-[250px]">
                     <template x-for="(category, index) in filteredCategories" :key="index">
                         <button type="button" x-on:click="category_id = category.id; search = ''"
-                            class="flex items-center w-full px-3 py-2 duration-200 ease-in-out rounded-md hover:bg-slate-100 dark:hover:bg-slate-800 justify-between" :class="{ 'bg-slate-100 dark:bg-slate-800': selectedCategoryName === category.name }">
+                            class="flex items-center justify-between w-full px-3 py-2 duration-200 ease-in-out rounded-md hover:bg-slate-100 dark:hover:bg-slate-800"
+                            :class="{ 'bg-slate-100 dark:bg-slate-800': selectedCategoryName === category.name }">
                             <span class="text-sm capitalize" x-text="category.name"></span>
 
                             <div x-cloak x-show="selectedCategoryName === category.name">
-                                <flux:icon.check class="h-5 -mr-1.5 w-5 p-0.5 text-slate-600 stroke-indigo-600 stroke-2 dark:text-slate-400" />
+                                <flux:icon.check
+                                    class="h-5 -mr-1.5 w-5 p-0.5 text-slate-600 stroke-indigo-600 stroke-2 dark:text-slate-400" />
                             </div>
                         </button>
                     </template>
