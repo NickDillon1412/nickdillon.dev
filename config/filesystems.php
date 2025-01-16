875ce4d630b4e10ee75bdc4c935a5e8ba70fb5ee
Nick Dillon <nickhds@gmail.com>
2025-01-15 21:34:38 -0600

    Fix scheme

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
-            'scheme' => app()->isProduction() ? 'https' : 'http'
+            'scheme' => env('APP_ENV') === 'production' ? 'https' : 'http'
         ],
 
     ],
