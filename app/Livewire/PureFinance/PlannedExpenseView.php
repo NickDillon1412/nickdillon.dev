8e7be99e0e098c44db903f7d683d7431302478d3
Nick Dillon <nickhds@gmail.com>
2025-02-08 13:56:06 -0600

    Add last 6 months chart

--- app/Livewire/PureFinance/PlannedExpenseView.php
+++ app/Livewire/PureFinance/PlannedExpenseView.php
@@ -6,12 +6,15 @@ use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Illuminate\Contracts\View\View;
 use App\Models\PureFinance\Transaction;
+use Illuminate\Database\Eloquent\Builder;
 use App\Models\PureFinance\PlannedExpense;
 
 #[Layout('layouts.app')]
 class PlannedExpenseView extends Component
 {
     public PlannedExpense $expense;
+
+    public string $timezone = '';
 
     public int|float $total_spent = 0;
 
@@ -21,29 +24,70 @@ class PlannedExpenseView extends Component
 
     public float $percentage_spent = 0;
 
-    public function render(): View
+    public array $monthly_totals = [];
+
+    public function mount(): void
     {
-        $timezone = 'America/Chicago';
+        $this->timezone = 'America/Chicago';
+    }
 
-        $start_of_month = now()->timezone($timezone)->startOfMonth()->toDateString();
+    private function getCurrentMonthData(): void
+    {
+        $start_of_month = now()->timezone($this->timezone)->startOfMonth()->toDateString();
 
-        $end_of_month = now()->timezone($timezone)->endOfMonth()->toDateString();
+        $end_of_month = now()->timezone($this->timezone)->endOfMonth()->toDateString();
 
-        $this->total_spent = Transaction::query()
-            ->whereCategoryId($this->expense->category_id)
-            ->orWhereRelation('category', 'parent_id', $this->expense->category_id)
-            ->whereBetween('date', [$start_of_month, $end_of_month])
-            ->sum('amount');
+        $transactions_query = Transaction::query()
+            ->where(function (Builder $query): void {
+                $query->where('category_id', $this->expense->category_id)
+                    ->orWhereRelation('category', 'parent_id', $this->expense->category_id);
+            })
+            ->whereBetween('date', [$start_of_month, $end_of_month]);
 
-        $this->transaction_count = Transaction::query()
-            ->whereCategoryId($this->expense->category_id)
-            ->orWhereRelation('category', 'parent_id', $this->expense->category_id)
-            ->whereBetween('date', [$start_of_month, $end_of_month])
-            ->count();
+        $this->total_spent = $transactions_query->sum('amount');
+
+        $this->transaction_count = $transactions_query->count();
 
         $this->available = $this->expense->monthly_amount - $this->total_spent;
 
         $this->percentage_spent = ($this->total_spent / $this->expense->monthly_amount) * 100;
+    }
+
+    private function getTotalSpentLastSixMonths(): void
+    {
+        $now = now()->timezone($this->timezone);
+
+        $months = collect();
+
+        for ($i = 1; $i < 7; $i++) {
+            $months->push($now->copy()->subMonths($i));
+        }
+
+        foreach ($months as $month) {
+            $start_of_month = $month->startOfMonth()->toDateString();
+            $end_of_month = $month->endOfMonth()->toDateString();
+
+            // Sum the amount for transactions in the selected category and month
+            $total_for_month = Transaction::query()
+                ->where(function (Builder $query): void {
+                    $query->where('category_id', $this->expense->category_id)
+                        ->orWhereRelation('category', 'parent_id', $this->expense->category_id);
+                })
+                ->whereBetween('date', [$start_of_month, $end_of_month])
+                ->sum('amount');
+
+            $this->monthly_totals[] = [
+                'month' => $month->format('M'),
+                'total_spent' => ceil($total_for_month),
+            ];
+        }
+    }
+
+    public function render(): View
+    {
+        $this->getCurrentMonthData();
+
+        $this->getTotalSpentLastSixMonths();
 
         return view('livewire.pure-finance.planned-expense-view');
     }
--- resources/views/livewire/pure-finance/planned-expense-view.blade.php
+++ resources/views/livewire/pure-finance/planned-expense-view.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ plannedSpendingFormModalOpen: false }" x-init="console.log(Intl.DateTimeFormat().resolvedOptions().timeZone)" x-on:planned-expense-saved.window="$wire.$refresh"
+<div x-data="{ plannedSpendingFormModalOpen: false }" x-on:planned-expense-saved.window="$wire.$refresh"
     class="w-full p-4 mx-auto overflow-y-hidden sm:py-8 sm:px-6 lg:px-8 max-w-screen-2xl">
     <div class="flex flex-col space-y-3">
         <h1 class="text-2xl font-semibold md:text-3xl text-slate-800 dark:text-slate-200">
@@ -22,62 +22,105 @@
                 </div>
             </div>
 
-            <div class="flex flex-col border-t border-slate-200 dark:border-slate-600">
-                <div class="flex flex-col px-4 py-3 space-y-2 text-sm">
-                    <h3 class="font-medium">
-                        Spending in category: {{ $expense->category->name }}
-                    </h3>
+            <div class="border-t border-slate-200 dark:border-slate-600">
+                <h3 class="px-4 pt-3 text-sm font-medium">
+                    Spending in category: {{ $expense->category->name }}
+                </h3>
 
-                    <div>
-                        <p>Available</p>
-
-                        <div class="flex items-center justify-between">
-                            <h3 @class([
-                                '!text-red-500' => $percentage_spent > 100,
-                                'font-semibold',
-                            ])>
-                                @if ($percentage_spent <= 100)
-                                    ${{ Number::format($available ?? 0, 2) }}
-                                @else
-                                    ${{ Number::format(abs($available) ?? 0, 2) }} over spent
-                                @endif
+                <div class="flex flex-col justify-between sm:space-x-8 sm:flex-row">
+                    <div class="flex flex-col w-full">
+                        <div class="flex flex-col px-4 py-3 space-y-2 text-sm">
+                            <h3 class="font-medium uppercase">
+                                This Month
                             </h3>
 
-                            <p>
-                                {{ $transaction_count }} {{ Str::plural('transaction', $transaction_count) }}
-                            </p>
-                        </div>
+                            <div>
+                                <p>Available</p>
 
-                        <div class="w-full my-1.5 h-9 bg-indigo-100 dark:bg-slate-700 shadow-sm rounded-lg">
-                            <div @class([
-                                '!rounded-r-lg' => $percentage_spent >= 100,
-                                '!bg-red-500' => $percentage_spent > 100,
-                                'min-w-[25px]' => $percentage_spent > 0,
-                                '!bg-transparent' => $percentage_spent === 0,
-                                'flex items-center justify-center h-full bg-indigo-500 rounded-lg rounded-r-none',
-                            ]) style="width: {{ min($percentage_spent, 100) }}%;">
-                                <span x-cloak x-show="$wire.percentage_spent > 0" class="font-semibold text-white">
-                                    {{ Number::format($percentage_spent, 0) }}%
-                                </span>
+                                <div class="flex items-center justify-between">
+                                    <h3 @class([
+                                        '!text-red-500' => $percentage_spent > 100,
+                                        'font-semibold',
+                                    ])>
+                                        @if ($percentage_spent <= 100)
+                                            ${{ Number::format($available ?? 0, 2) }}
+                                        @else
+                                            ${{ Number::format(abs($available) ?? 0, 2) }} over spent
+                                        @endif
+                                    </h3>
+
+                                    <p>
+                                        {{ $transaction_count }} {{ Str::plural('transaction', $transaction_count) }}
+                                    </p>
+                                </div>
+
+                                <div class="w-full my-1.5 h-9 bg-indigo-100 dark:bg-slate-700 shadow-sm rounded-lg">
+                                    <div @class([
+                                        '!rounded-r-lg' => $percentage_spent >= 100,
+                                        '!bg-red-500' => $percentage_spent > 100,
+                                        'min-w-[25px]' => $percentage_spent > 0,
+                                        '!bg-transparent' => $percentage_spent === 0,
+                                        'flex items-center justify-center h-full bg-indigo-500 rounded-lg rounded-r-none',
+                                    ])
+                                        style="width: {{ min($percentage_spent, 100) }}%;">
+                                        <span x-cloak x-show="$wire.percentage_spent > 0"
+                                            class="font-semibold text-white">
+                                            {{ Number::format($percentage_spent, 0) }}%
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="flex items-center justify-between">
+                                    <p>
+                                        Spent
+
+                                        <span class="font-semibold">
+                                            ${{ Number::format($total_spent ?? 0, 2) }}
+                                        </span>
+                                    </p>
+
+                                    <p>
+                                        of
+
+                                        <span class="font-semibold">
+                                            ${{ Number::format($expense->monthly_amount ?? 0, 2) }}
+                                        </span>
+                                    </p>
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="flex items-center justify-between">
-                            <p>
-                                Spent
+                    <div class="w-full px-4 py-3">
+                        <h3 class="mb-2 text-sm font-medium uppercase">
+                            Last 6 Months
+                        </h3>
 
-                                <span class="font-semibold">
-                                    ${{ Number::format($total_spent ?? 0, 2) }}
-                                </span>
-                            </p>
+                        <div class="relative flex flex-col my-8 text-sm">
+                            <div class="flex items-end mb-2">
+                                <template x-for="month in $wire.monthly_totals.reverse()">
+                                    <div class="w-[45px] mx-2 sm:mx-3">
+                                        <div :style="`height: ${month.total_spent}px`"
+                                            class="relative bg-indigo-500 shadow-sm rounded-t-md">
+                                            <div x-text="`$${month.total_spent}`"
+                                                class="absolute top-0 left-0 right-0 -mt-6 text-sm text-center">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </template>
+                            </div>
 
-                            <p>
-                                of
-
-                                <span class="font-semibold">
-                                    ${{ Number::format($expense->monthly_amount ?? 0, 2) }}
-                                </span>
-                            </p>
+                            <div class="flex items-end">
+                                <template x-for="month in $wire.monthly_totals">
+                                    <div class="w-[45px] mx-2 sm:mx-3">
+                                        <div class="relative">
+                                            <div x-text="month.month"
+                                                class="absolute top-0 left-0 right-0 mt-1 text-sm text-center">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </template>
+                            </div>
                         </div>
                     </div>
                 </div>
